29486fcc255216a571ce1a5b6eaa9a9c7a2afef0
Novimatrem <thenovimatrem@protonmail.ch>
2024-09-21 02:31:55 +0100

    steam-machine-too-dark-fix

---
+++ steam-machine-too-dark-fix.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# steam-machine-too-dark-fix.sh
+
+killall redshift-gtk
+killall redshift
+pkill redshift
+pkill redshift-gtk
+clear
+
+while true; do sleep 0.01s && xgamma -gamma 1.45 && echo "" && echo "" && echo "" && echo "" && echo "" && echo "xgamma -gamma 1.45" && echo ""; done
+
+xgamma -gamma 1.45
+
+
