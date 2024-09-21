00420b1153303b84b7b1c75dadb86177ab1473f8
Novimatrem <69379708+Novimatrem@users.noreply.github.com>
2024-09-21 05:56:03 +0100

    Update steam-machine-too-dark-fix.sh

--- steam-machine-too-dark-fix.sh
+++ steam-machine-too-dark-fix.sh
@@ -7,7 +7,9 @@ pkill redshift
 pkill redshift-gtk
 clear
 
-while true; do sleep 1s && xgamma -gamma 1.45 && echo "" && echo "" && echo "" && echo "" && echo "" && echo "xgamma -gamma 1.45" && echo ""; done
+# yes while the sleep 0s here wastes performance, it is required due to bugs.
+
+while true; do sleep 0s && xgamma -gamma 1.45 && echo "" && echo "" && echo "" && echo "" && echo "" && echo "xgamma -gamma 1.45" && echo ""; done
 
 xgamma -gamma 1.45
 
